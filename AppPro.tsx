0883d9cb51054472d629e9cdc29d64b15cdaea5c
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-11-15 18:07:45 +0530

    AwesomeApp

---
+++ AppPro.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme
+} from 'react-native'
+
+function AppPro(): JSX.Element{
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return(
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.darkText }>
+                Hello World
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    whiteText:{
+        color:'#ffffff'
+    },
+    darkText:{
+        color:'#000000'
+    }
+})
+
+export default AppPro;
